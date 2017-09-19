4354eb0bd27e68690720c7d15c495aa6177d38e6
nahumrosillo <nahum.rosillolopez@gmail.com>
2017-09-19 18:55:20 +0200

    Reformated code to standard java code style

---
+++ Quizzy/app/src/main/java/com/quizzy/nahum/Question.java
@@ -0,0 +1,32 @@
+package com.quizzy.nahum;
+
+/**
+ * Created by nahum on 19/09/17.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
--- Quizzy/app/src/main/java/com/quizzy/nahum/QuizzyActivity.java
+++ Quizzy/app/src/main/java/com/quizzy/nahum/QuizzyActivity.java
@@ -7,34 +7,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class QuizzyActivity extends AppCompatActivity
-{
+public class QuizzyActivity extends AppCompatActivity {
     private Button mTrueButton, mFalseButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizzy);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
 
-        mTrueButton.setOnClickListener(new View.OnClickListener()
-        {
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 Toast.makeText(QuizzyActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
             }
         });
         
         mFalseButton = (Button) findViewById(R.id.false_button);
 
-        mFalseButton.setOnClickListener(new View.OnClickListener()
-        {
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 Toast.makeText(QuizzyActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
             }
         });
