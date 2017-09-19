f1dedf8628dd29d9234d26a77784a4d09c90fff9
nahumrosillo <nahum.rosillolopez@gmail.com>
2017-09-19 18:40:39 +0200

    Unique question

---
+++ Quizzy/app/src/main/java/com/quizzy/nahum/QuizzyActivity.java
@@ -0,0 +1,42 @@
+package com.quizzy.nahum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class QuizzyActivity extends AppCompatActivity
+{
+    private Button mTrueButton, mFalseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quizzy);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(QuizzyActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        mFalseButton = (Button) findViewById(R.id.false_button);
+
+        mFalseButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(QuizzyActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
