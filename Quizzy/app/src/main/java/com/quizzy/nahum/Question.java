8350dbbad4cc212f685e3c1275a36c060b554af7
nahumrosillo <nahum.rosillolopez@gmail.com>
2017-09-23 18:06:26 +0200

    Add unique answer

--- Quizzy/app/src/main/java/com/quizzy/nahum/Question.java
+++ Quizzy/app/src/main/java/com/quizzy/nahum/Question.java
@@ -8,10 +8,12 @@ public class Question {
 
     private int mTextResId;
     private boolean mAnswerTrue;
+    private boolean mAnswered;
 
     public Question(int textResId, boolean answerTrue) {
         mTextResId = textResId;
         mAnswerTrue = answerTrue;
+        mAnswered = false;
     }
 
     public int getTextResId() {
@@ -28,5 +30,13 @@ public class Question {
 
     public void setAnswerTrue(boolean answerTrue) {
         mAnswerTrue = answerTrue;
+    }
+
+    public boolean isAnswered() {
+        return mAnswered;
+    }
+
+    public void setAnswered(boolean answered) {
+        mAnswered = answered;
     }
 }
--- Quizzy/app/src/main/java/com/quizzy/nahum/QuizzyActivity.java
+++ Quizzy/app/src/main/java/com/quizzy/nahum/QuizzyActivity.java
@@ -2,6 +2,7 @@ package com.quizzy.nahum;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizzyActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizzyActivity";
+    private static final String KEY_INDEX = "index";
+
+    private int corrects = 0;
+
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
@@ -17,15 +24,25 @@ public class QuizzyActivity extends AppCompatActivity {
     private Question[] mQuestionBank = new Question[] {
             new Question(R.string.question_spain, true),
             new Question(R.string.question_france, true),
-            new Question(R.string.question_portugal, true)
+            new Question(R.string.question_portugal, false)
     };
 
     private int mCurrentIndex = 0;
 
     @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.e(TAG, "onSaveInstanceState");
+        outState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizzy);
+
+        if (savedInstanceState != null)
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
 
@@ -51,6 +68,11 @@ public class QuizzyActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+
+                if (mCurrentIndex == mQuestionBank.length-1) {
+                    showPercentage();
+                }
+
                 updateQuestion();
             }
         });
@@ -63,15 +85,35 @@ public class QuizzyActivity extends AppCompatActivity {
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
+
+        checkAnswered();
     }
 
+
+    private void checkAnswered() {
+
+        if (mQuestionBank[mCurrentIndex].isAnswered()) {
+            mFalseButton.setEnabled(false);
+            mTrueButton.setEnabled(false);
+        } else {
+            mFalseButton.setEnabled(true);
+            mTrueButton.setEnabled(true);
+        }
+    }
+
+
     private void checkAnswer(boolean userPressedTrue) {
+
+        mQuestionBank[mCurrentIndex].setAnswered(true);
+        checkAnswered();
+
         boolean answerTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
 
         int messageResId = 0;
 
         if (userPressedTrue == answerTrue) {
             messageResId = R.string.correct_toast;
+            corrects++;
         } else {
             messageResId = R.string.incorrect_toast;
         }
@@ -79,5 +121,11 @@ public class QuizzyActivity extends AppCompatActivity {
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 
+    private void showPercentage() {
+        int total = mQuestionBank.length;
 
+        Toast.makeText(QuizzyActivity.this, corrects+"/"+total, Toast.LENGTH_SHORT).show();
+
+    }
 }
+
